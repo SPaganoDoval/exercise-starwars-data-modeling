b62af8a44ae209ffdc2d50ca811d0c044f95a6b1
SPaganoDoval <dovalsonia@gmail.com>
2023-02-22 21:16:46 +0000

    para entregar

--- src/models.py
+++ src/models.py
@@ -8,23 +8,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+    username = Column(String(50),unique=True, nullable=False)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250),unique=True, nullable=False)
+    password = Column(String(25),unique=True, nullable=False)
+    favorite_planet = Column(String(250),unique=True, nullable=False)
+    favorite_starship = Column(String(250),unique=True, nullable=False)
+    favorite_character = Column(String(250),unique=True, nullable=False)
 
-class User(Base):
-    __tablename__ = 'user'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    diameter = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    gravity = Column(Integer, nullable=False)
+
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    height = Column(Integer, nullable=False)
+    hair_color = Column(String(50), nullable=False)
+    skin_color = Column(String(50), nullable=False)
+    eye_color = Column(String(50), nullable=False)
+
+class Starship(Base):
+    __tablename__ = 'character'
+    startship_class = Column(String(250))
+    length = Column(String(25), Integer)
+    pilots = Column(String(250), nullable=False)
+   
 
     def to_dict(self):
         return {}
